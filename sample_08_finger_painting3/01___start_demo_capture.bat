b139ba84f53bf60f0203c0f3b9a6c1818c0cfc1c
bradgrantham-lunarg <brad@lunarg.com>
2025-06-03 15:29:50 -0600

    Remove 500 frame limit on capture

--- sample_08_finger_painting3/01___start_demo_capture.bat
+++ sample_08_finger_painting3/01___start_demo_capture.bat
@@ -3,10 +3,13 @@ adb shell "settings put global gpu_debug_app net.beyondreality.openxr_provider.f
 adb shell "settings put global gpu_debug_layers 'VK_LAYER_LUNARG_gfxreconstruct'"
 
 adb shell "setprop debug.gfxrecon.capture_file  '/sdcard/Download/openxr_capture.gfxr'"
-adb shell "setprop debug.gfxrecon.capture_frames '1-500'"
 adb shell "setprop debug.gfxrecon.capture_file_timestamp 0"
+
+REM adb shell "setprop debug.gfxrecon.capture_frames '1-500'"
+adb shell "setprop debug.gfxrecon.capture_frames ''"
 
 adb shell "setprop debug.gfxrecon.skip_threads_with_invalid_data  '1'"
 
-adb shell rm /sdcard/Download/openxr_capture_frames_1_through_500.gfxr
+adb shell rm /sdcard/Download/openxr_capture*.gfxr
+
 adb shell monkey -p net.beyondreality.openxr_provider.finger_painting3.openxr -c android.intent.category.LAUNCHER 1
--- sample_08_finger_painting3/03___run_replay.bat
+++ sample_08_finger_painting3/03___run_replay.bat
@@ -1,3 +1,6 @@
 cd ..\..\gfxreconstruct\android
-python3 scripts\gfxrecon.py replay /sdcard/Download/openxr_capture_frames_1_through_500.gfxr
+
+python3 scripts\gfxrecon.py replay /sdcard/Download/openxr_capture.gfxr
+REM python3 scripts\gfxrecon.py replay /sdcard/Download/openxr_capture_frames_1_through_500.gfxr
+
 cd ..\..\OpenXRProvider_v2\sample_08_finger_painting3
